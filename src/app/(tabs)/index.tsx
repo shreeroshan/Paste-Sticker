8a65bc7a8a33326a4571ea2424c1ecf26be7df67
shreeroshan <shreeroshan60@gmail.com>
2026-05-25 15:23:40 +0545

    Button modified and imageViewer

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,13 +1,20 @@
 import { Link } from "expo-router";
 import { Text, View, StyleSheet } from "react-native";
+import ImageViewer from "@/components/ImageViewer";
+import Button from "@/components/Button";
 
+
+const PlaceholderImage=require("@/assets/sticker-smash-assets/images/background-image.png")
 export default function Index() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Home Screen.</Text>
-      <Link href={"/about"} style={styles.button}>
-      Go to About Screen
-      </Link>
+      <View style={styles.imageContainer}>
+     <ImageViewer imgSource={PlaceholderImage} />
+      </View>
+      <View style={styles.footerContainer}>
+        <Button theme="primary" label="Choose a photo"/>
+        <Button label="Use this photo"/>
+      </View>
     </View>
   );
 }
@@ -27,5 +34,17 @@ const styles = StyleSheet.create({
     justifyContent:"center",
     fontSize:20,
     textDecorationLine:"underline"
+  },
+  imageContainer:{
+    flex:1,
+  },
+  image:{
+    width:320,
+    height:440,
+    borderRadius:18
+  },
+  footerContainer:{
+    flex:1/3,
+    alignItems:"center"
   }
 }); 
