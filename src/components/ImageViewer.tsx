05c2063a2ebe1d8cebbe5a983d9f04811a06e5c1
shreeroshan <shreeroshan60@gmail.com>
2026-05-25 17:06:04 +0545

    emoji

--- src/components/ImageViewer.tsx
+++ src/components/ImageViewer.tsx
@@ -3,11 +3,13 @@ import { ImageSourcePropType, StyleSheet } from 'react-native';
 
 type Props={
     imgSource: ImageSourcePropType
+    selectedImage?:string
 }
 
 
-export default function ImageViewer({imgSource}:Props){
-    return <Image source={imgSource} style={styles.image}/>
+export default function ImageViewer({imgSource, selectedImage}:Props){
+   const ImageSource=selectedImage?{uri:selectedImage}:imgSource
+    return <Image source={ImageSource} style={styles.image}/>
 }
 
 const styles=StyleSheet.create({
